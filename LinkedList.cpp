94c0a616bb6f1e61eafc5a77e1caa6a8a6aec9b4
Sarvesh-stu <s3946533@student.rmit.edu.au>
2023-04-30 19:32:52 +1000

    function stubs for the doubly linkedList

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,11 +1,56 @@
 #include "LinkedList.h"
 
 LinkedList::LinkedList() {
-   head = nullptr;
-
-   // TODO
+    head = nullptr;
+    tail = nullptr;
+    listLength = 0;
 }
 
+void LinkedList::add(Stock info){
+    
+}
+
+void LinkedList::remove(std::string id){
+
+}
+
+void LinkedList::print(){
+
+}
+
+unsigned int LinkedList::getListLength(){
+    return listLength;
+}
+
+unsigned int LinkedList::getStockLevels(std::string id){
+
+}
+
+std::string LinkedList::getName(std::string id){
+
+}
+
+std::string LinkedList::getDescription(std::string id){
+
+}
+
+Price LinkedList::getPrice(std::string id){
+
+}
+
+void LinkedList::restock(){
+
+}
+
+void LinkedList::buy(std::string id){
+
+}
+
+void LinkedList::deleteList(){
+
+}
+
+
 LinkedList::~LinkedList() {
-    // TODO
+    deleteList();
 }
--- LinkedList.h
+++ LinkedList.h
@@ -10,13 +10,26 @@ public:
     ~LinkedList();
 
     // more functions to be added perhaps...
+    void add(Stock info);
+    void remove(std::string id);
+    void print();
+    unsigned int getListLength();
+    unsigned int  getStockLevels(std::string id);
+    std::string getName(std::string id);
+    std::string getDescription(std::string id);
+    Price getPrice(std::string id);
+    void restock();
+    void buy(std::string id);
+    void deleteList();
+
 
 private:
     // the beginning of the list
     Node* head;
+    Node* tail;
   
     // how many nodes are there in the list?
-    unsigned count;
+    unsigned listLength;
 };
 
 #endif  // LINKEDLIST_H
